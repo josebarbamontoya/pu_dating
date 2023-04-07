66a4510f035e0dbb319fad187038a6c7e8f44500
Jose Barba-Montoya <38330595+josebarbamontoya@users.noreply.github.com>
2023-04-07 18:54:01 -0400

    Add files via upload

---
+++ reltime-ja_tutorial/4_assess_pu_impact/megacc_code.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+megacc_12_221011_macOS -a analyze_user_tree_ML_nucleotide.mao -d align_prun.fas -t tree_prun.nwk -o ml_out
+
---
+++ reltime-ja_tutorial/4_assess_pu_impact/reltime_code.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+megacc_12_221011_macOS -a reltimeFromBranchLengths.mao -t blens_prun.nwk -g outgroup_prun.txt -c calibrations.txt -o out 
+
